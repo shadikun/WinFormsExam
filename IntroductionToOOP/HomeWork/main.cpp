3f91eb56b1d039e2926833fdcd52f6c4d82ee88b
Alex <Alex@201-1>
2019-12-22 13:54:13 +0200

    String

--- IntroductionToOOP/HomeWork/main.cpp
+++ IntroductionToOOP/HomeWork/main.cpp
@@ -1,237 +1,42 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
-class S_Fraction
+class String
 {
 private:
-	int Numerator;
-	int Denominator;
-	int WholePart;
+	char* pStr;
+	int _size;
 public:
-	S_Fraction(int n = 0, int d = 1, int w = 0)
+	String()
 	{
-		this->Numerator = n;
-		this->Denominator = d;
-		this->WholePart = w;
-		cout << "Constructor:\t" << this << endl;
+		pStr = NULL;
+		_size = 0;
 	}
-	~S_Fraction()
+	String(const char* str)
 	{
-		cout << "Destructor:\t" << this << endl;
+		_size = strlen(str) + 1;
+		pStr = new char[_size];
+		strcpy(pStr, str);
 	}
-
-	int getNumerator() const
+	int size()
 	{
-		return Numerator;
+		return _size;
 	}
-	int getDenominator() const
+	~String()
 	{
-		return Denominator;
+		delete[] pStr;
 	}
-	int getWholePart() const
+	friend ostream& operator << (ostream& os, String& obj)
 	{
-		return WholePart;
+		//for (int i = 0; i < obj.size(); i++)
+			os << obj.pStr;
+		return os;
 	}
-
-	double get_decimal()	// получение дроби в типе double для сравнений на <, <=, >, >=
-	{
-		double result = (double)(WholePart * Denominator + Numerator) / Denominator;
-		return result;
-	}
-	void set_Numerator(int n)
-	{
-		this->Numerator = n;
-	}
-	void set_Denominator(int d)
-	{
-		if (d == 0)
-		{
-			cout << "Знаменатель не может быть нулем. На ноль делить нельзя!" << endl;
-			while (d <= 0)
-			{
-				cout << "Введите новое положительное значение знаменателя: ";
-				cin >> d;
-				cout << endl;
-			}
-			this->Denominator = d;
-		}
-		else
-		{
-			this->Denominator = d;
-		}
-	}
-	void set_Whole_part(int w)
-	{
-		if (Numerator >= Denominator)
-		{
-			this->WholePart = w;
-			WholePart += (Numerator / Denominator);
-			Numerator %= Denominator;
-		}
-		else this->WholePart = 0;
-	}
-
-	void getIntPart()	//выделение целой части дроби
-	{
-		if (Numerator >= Denominator)
-		{
-			WholePart += (Numerator / Denominator);
-			Numerator %= Denominator;
-		}
-	}
-	void Cancellation()	//сокращение дроби
-	{
-		if (Numerator != 0)
-		{
-			int m = Denominator,
-				n = Numerator,
-				rest = m % n;
-			while (rest != 0)
-			{
-				m = n; n = rest;
-				rest = m % n;
-			}
-			int nod = n;
-			if (nod != 1)
-			{
-				Numerator /= nod; Denominator /= nod;
-			}
-		}
-	}
-	void GetMixedView()				//выделение целой части + сокращение дроби
-	{
-		getIntPart();
-		Cancellation();
-	}
-
-	S_Fraction& operator=(const S_Fraction& other)
-	{
-		this->Numerator = other.Numerator;
-		this->Denominator = other.Denominator;
-		this->WholePart = other.WholePart;
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-	S_Fraction& operator+(const S_Fraction& other)
-	{
-		S_Fraction result;
-		result.Numerator = (this->WholePart*this->Denominator + this->Numerator)*other.Denominator +
-			(other.WholePart*other.Denominator + other.Numerator)*this->Denominator;
-		result.Denominator = this->Denominator*other.Denominator;
-		result.GetMixedView();
-		return result;
-	}
-	S_Fraction& operator-(const S_Fraction& other)
-	{
-		S_Fraction result;
-		result.Numerator = (this->WholePart*this->Denominator + this->Numerator)*other.Denominator -
-			(other.WholePart*other.Denominator + other.Numerator)*this->Denominator;
-		result.Denominator = this->Denominator*other.Denominator;
-		result.GetMixedView();
-		return result;
-	}
-	S_Fraction operator*(const S_Fraction& other)
-	{
-		S_Fraction result;
-		result.Numerator = (this->WholePart*this->Denominator + this->Numerator)*
-			(other.WholePart*other.Denominator + other.Numerator);
-		result.Denominator = this->Denominator*other.Denominator;
-		result.GetMixedView();
-		return result;
-	}
-	S_Fraction operator/(const S_Fraction& other)
-	{
-		S_Fraction result;
-		result.Numerator = (this->WholePart*this->Denominator + this->Numerator)*other.Denominator;
-		result.Denominator = this->Denominator*(other.WholePart*other.Denominator + other.Numerator);
-		result.GetMixedView();
-		return result;
-	}
-
-	bool operator ==(const S_Fraction& other)
-	{
-		if (this->WholePart != other.WholePart || this->Numerator * other.Denominator !=
-			this->Denominator * other.Numerator)
-			return false;
-		return true;
-	}
-	bool operator !=(const S_Fraction& other)
-	{
-		if (this->WholePart == other.WholePart && this->Numerator * other.Denominator ==
-			this->Denominator * other.Numerator)
-			return false;
-		return true;
-	}
-	bool operator > (const S_Fraction& other)
-	{
-		double result1 = (double)(this->WholePart * this->Denominator + this->Numerator) / this->Denominator;
-		double result2 = (double)(other.WholePart * other.Denominator + other.Numerator) / other.Denominator;
-		if (result1 <= result2) return false;
-		else return true;
-	}
-	bool operator < (const S_Fraction& other)
-	{
-		double result1 = (double)(this->WholePart * this->Denominator + this->Numerator) / this->Denominator;
-		double result2 = (double)(other.WholePart * other.Denominator + other.Numerator) / other.Denominator;
-		if (result1 >= result2) return false;
-		else return true;
-	}
-	bool operator >= (const S_Fraction& other)
-	{
-		double result1 = (double)(this->WholePart * this->Denominator + this->Numerator) / this->Denominator;
-		double result2 = (double)(other.WholePart * other.Denominator + other.Numerator) / other.Denominator;
-		if (result1 < result2) return false;
-		else return true;
-	}
-	bool operator <= (const S_Fraction& other)
-	{
-		double result1 = (double)(this->WholePart * this->Denominator + this->Numerator) / this->Denominator;
-		double result2 = (double)(other.WholePart * other.Denominator + other.Numerator) / other.Denominator;
-		if (result1 > result2) return false;
-		else return true;
-	}
-
-	friend ostream& operator << (ostream& out, S_Fraction a);
 };
-	ostream& operator << (ostream& out, S_Fraction a)
-	{
-		if (a.getWholePart() != 0)
-			out << a.getWholePart() << " ";
-		if (a.getNumerator() != 0)
-			out << a.getNumerator() << "/" << a.getDenominator();
-		if (a.getWholePart() == 0 && a.getNumerator() == 0)
-			out << "0";
-		else out << " ";
-		return out;
-	}
 
-
-
-void main()
+int main(int argc, char* argv[])
 {
-	setlocale(LC_ALL, "");
-	S_Fraction A;
-	A.set_Numerator(5);
-	A.set_Denominator(10);
-	A.set_Whole_part(0);
-	//cout << "Вывод объекта А в виде простой дроби: "<<A<<endl;
-
-
-	S_Fraction B;
-	B.set_Numerator(1);
-	B.set_Denominator(2);
-	B.set_Whole_part(0);
-	/*cout << "Вывод объекта B в виде простой дроби: " << B << endl;
-
-	cout << "Результат сложения двух дробей: "<<A+B<<endl;
-	cout << "Результат вычитания двух дробей: " << A - B << endl;
-	cout<< "Результат умножения двух дробей: " << A * B << endl;
-	cout << "Результат деления двух дробей: " << A / B << endl;
-	cout << "Дробь " << A << "равна " << B <<": "<<(A == B) << endl;
-	cout << "Дробь " << A << "неравна " << B << ": " << (A != B) << endl;
-	cout << "Дробь " << A << "неравна " << B << ": " << (A != B) << endl;*/
-	if (A >= B) cout << "Дробь " << A << "больше или равна дроби " << B << endl;
-	else cout << "Дробь " << A << "меньше дроби " << B << endl;
-
+	String str("Hellow");
+	cout << str << endl;;
+	cout << "Hello World" << std::endl;
+	return 0;
 }
